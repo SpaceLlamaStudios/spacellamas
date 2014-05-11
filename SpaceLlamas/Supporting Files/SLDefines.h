80c9b2701d8e25b665b2442c8d87e7376f166245
Elsie Ng <elsie.ng.design@gmail.com>
2014-05-10 22:46:06 -0400

    Added Defines and fade transition

---
+++ SpaceLlamas/Supporting Files/SLDefines.h
@@ -0,0 +1,14 @@
+//
+//  SLDefines.h
+//  SpaceLlamas
+//
+//  Created by Elsie Ng on 2014-05-10.
+//  Copyright (c) 2014 Space Llama Studios. All rights reserved.
+//
+
+#ifndef SpaceLlamas_SLDefines_h
+#define SpaceLlamas_SLDefines_h
+
+
+
+#endif
